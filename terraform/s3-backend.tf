6d0b0a3061b5f23e1a3a160fe6149970102ee0c4
xavier cordova <xaviercordovajr@gmail.com>
2021-06-11 11:28:22 -0500

    Update s3-backend.tf removed variable bucket name

--- terraform/s3-backend.tf
+++ terraform/s3-backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket = var.backend_name
+    bucket = "cloudresume-state"
     key    = "statefiles/website/terraform.tfstate"
     region = "us-east-1"
 #    dynamodb_table = "terraform-state-lock"
