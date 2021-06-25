b464586df25c2a63d69c54d2bdfc72c097e69e82
xavier cordova <xaviercordovajr@gmail.com>
2021-06-25 11:03:54 -0500

    Add favicon and api link
    Commit adds browser icon and api url

--- xhr.js
+++ xhr.js
@@ -1,39 +1,27 @@
-
-// window.onload = () => {
-//     console.log('window loaded');
-//     const getData = () => {
-//         const xhr = new XMLHttpRequest();
-//         // This prepares for a request to be sent. It does not make the request
-//         xhr.open('GET','https://reqres.in/api/users?page=2');
-        
-//         // This will accept json data and parse it into javascript data
-//         xhr.responseType = 'json';
-
-//         // When a responce loads this will trigger (xhr.onload) 
-//         xhr.onload = () => {
-//             const data = xhr.response;
-//             console.log(data);
-//         };
-//         // Sends the request that xhr.open set up
-//         xhr.send();
-//     };
-// }
-
 const xhr = new XMLHttpRequest(),
     method = "GET",
-    url = "https://reqres.in/api/users/2";
+    url = "https://jily262ica.execute-api.us-east-2.amazonaws.com/default/testfunction";
 
 xhr.open(method, url, true);
 xhr.onreadystatechange = function () {
   // In local files, status is 0 upon success in Mozilla Firefox
   if(xhr.readyState === XMLHttpRequest.DONE) {
     var status = xhr.status;
+    // if http responce is 0 or betwen 200-399 (200-299=successful responce 300-399=redirect)
     if (status === 0 || (status >= 200 && status < 400)) {
       // The request has been completed successfully
       console.log(xhr.responseText);
     } else {
-      // Oh no! There has been an error with the request!
+      console.log('Resource not found.');
     }
   }
 };
 xhr.send();
+
+// xhr has different stages of a request 0-4
+//   0 - unsent
+//   1 - opened
+//   2 - header recieved
+//   3 - loading
+//   4 - done
+
