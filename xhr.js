adcbc437786496239981a085020a7632848252cb
xavier cordova <xaviercordovajr@gmail.com>
2021-07-02 23:36:38 -0500

    Update website files
    Added visit counter element and fixed typo in html.
    Added api endpoint to xhr.js
    Minor movements in style.css

--- xhr.js
+++ xhr.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest(),
     method = "GET",
-    url = "https://jily262ica.execute-api.us-east-2.amazonaws.com/default/testfunction";
+    url = "https://f3n83h2nng.execute-api.us-east-1.amazonaws.com/prod/lambda-visit-count-function";
 
 xhr.open(method, url, true);
 xhr.onreadystatechange = function () {
@@ -10,7 +10,8 @@ xhr.onreadystatechange = function () {
     // if http responce is 0 or betwen 200-399 (200-299=successful responce 300-399=redirect)
     if (status === 0 || (status >= 200 && status < 400)) {
       // The request has been completed successfully
-      console.log(xhr.responseText);
+      //console.log(xhr.responseText);
+      document.getElementById("count").innerHTML=(xhr.responseText);
     } else {
       console.log('Resource not found.');
     }
@@ -18,10 +19,4 @@ xhr.onreadystatechange = function () {
 };
 xhr.send();
 
-// xhr has different stages of a request 0-4
-//   0 - unsent
-//   1 - opened
-//   2 - header recieved
-//   3 - loading
-//   4 - done
 
